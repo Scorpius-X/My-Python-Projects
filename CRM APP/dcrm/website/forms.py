e895e13b0a01fd46a34f18633b9d0d85633791d0
Eric Bolade <eric.bolade@gmail.com>
2025-11-22 15:57:59 +0100

    forms and register

---
+++ CRM APP/dcrm/website/forms.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib.auth.models import User
+from django import forms
+
+class signupForm(UserCreationForm):
+    emails = forms.EmailField(label="", widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+    firstname = forms.CharField(label="", max_length=150, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+    lastname = forms.CharField(label="", max_length=150, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+
+    class Meta:
+        model = User
+        fields = ('firstname', 'lastname', 'username', 'emails', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(signupForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+        
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<span class="form-text text-muted"><small>Your password must contain at least 8 characters.</small></span>'
+        
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
--- CRM APP/dcrm/website/views.py
+++ CRM APP/dcrm/website/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def home(request):
-    return render(request, 'home.html', {})
-# Create your views here.
+    # check if user is authenticated
+    if request.method == "POST":
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '')
+
+        #Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,f"Welcome back, {user.username}!")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})    
+
+def register_user(request):
+    return render(request, 'register.html', {})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home')
